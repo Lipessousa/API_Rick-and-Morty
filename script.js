3f7d6bd1e5c06ba1bcd111dfea5adab44e888e53
Lipessousa <88160166+Lipessousa@users.noreply.github.com>
2022-03-10 13:58:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function findPers(){    
+    let personagem = document.getElementById('personagem').value
+    var id = document.getElementById('idPer').value
+
+    let urlAPI = `https://rickandmortyapi.com/api/character/?name=${personagem}`
+
+
+
+    fetch(urlAPI)
+    .then(resp =>  resp.json())
+    .then((conteudo) =>{
+        console.log(conteudo.results[id])
+        document.getElementById('img-personagem').setAttribute("src", conteudo.results[id].image)
+        document.getElementById('name').innerText = `name: ${conteudo.results[id].name}`
+        document.getElementById('status').innerText = `status: ${conteudo.results[id].status}`
+        document.getElementById('origin').innerText = `origin: ${conteudo.results[id].origin.name}`
+        document.getElementById('species').innerText = `species: ${conteudo.results[id].species}`
+        document.getElementById('location').innerText = `location: ${conteudo.results[id].location.name}`
+    })
+}
+
+
+
